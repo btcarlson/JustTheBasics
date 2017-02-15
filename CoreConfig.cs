a200f99f64a487fe3ba1ab55269bbd26f5406b07
Khionu Sybiern <abstractkitty@iamtrue.net>
2017-02-14 20:26:44 -0500

    Created CoreConfig

---
+++ CoreConfig.cs
@@ -0,0 +1,30 @@
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace JustTheBasics
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class CoreConfig
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; } = "YOUR TOKEN HERE";
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; } = "!";
+        [JsonProperty("owner-ids")]
+        public ulong[] OwnerIds { get; set; } = new ulong[0];
+
+        public static CoreConfig ReadConfig()
+        {
+            if (!File.Exists("config.json"))
+            {
+                File.WriteAllText("config.json", JsonConvert.SerializeObject(new CoreConfig()));
+                throw new FileNotFoundException("Config File Not Found - Generating a template at 'config.json'");
+            }
+
+            var jsonString = File.ReadAllText("config.json");
+            
+            return JsonConvert.DeserializeObject<CoreConfig>(jsonString);
+        }
+    }
+}
